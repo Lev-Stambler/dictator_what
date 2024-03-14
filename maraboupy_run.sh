5fffa0f008f60fa3c84f6b847e188293f017dac7
Lev Stambler <levstamb@gmail.com>
2024-03-14 17:33:14 -0400

    remove slicing

--- maraboupy_run.sh
+++ maraboupy_run.sh
@@ -1,4 +1,4 @@
 git pull
-jupyter nbconvert --to script maraboupy_run.ipynb
+jupyter nbconvert --to script marboupy_test.ipynb
 export PYTHONPATH="$PYTHONPATH:/home/lev/code/research/ai/dictator/Marabou"
 python maraboupy_run.py
